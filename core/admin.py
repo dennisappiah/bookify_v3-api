1847cbc497369f35e1901e5b3c38ce30ce7ef28c
dennisappiahALX <appiahdennis1998@gmail.com>
2023-06-02 15:43:11 +0000

    extend the user model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core.models import User
+
+
+"""Extending the userAdmin model to include email, firstname, lastname"""
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "email", "first_name", "last_name"),
+            },
+        ),
+    )
+
