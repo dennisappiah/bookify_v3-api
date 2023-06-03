e8c430b9b47dcf94e08854c82533a9a85c82ba54
dennisappiahALX <appiahdennis1998@gmail.com>
2023-06-03 20:02:18 +0000

    map out urls

--- bookify/urls.py
+++ bookify/urls.py
@@ -5,6 +5,8 @@ from . import views
 router = routers.DefaultRouter()
 router.register('v1/categories', views.CategoryViewSet)
 router.register('v1/books', views.BookViewSet, basename="books")
+router.register('v1/customers', views.CustomerViewSet)
+router.register('v1/rentals', views.RentalViewSet)
 
 book_router = routers.NestedDefaultRouter(router, 'v1/books', lookup='book')
 book_router.register('comments', views.CommentViewSet, basename='book-comments')
