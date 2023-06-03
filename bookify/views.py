ca9b44ff9b3eb9a60a23c4e8acc7341976740132
dennisappiahALX <appiahdennis1998@gmail.com>
2023-06-03 20:01:21 +0000

    implement viewsets for endpoints

--- bookify/views.py
+++ bookify/views.py
@@ -1,11 +1,16 @@
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
-from .models import Category, Book, Comment, BookImage
+from .models import Category, Book, Comment, BookImage, Customer, Rental
 from .serializers import CategorySerializer,\
- BookSerializer, AddBookSerializer, CommentSerializer, BookImageSerializer
+ BookSerializer, AddBookSerializer, CommentSerializer,\
+  BookImageSerializer, CustomerSerializer, RentalSerializer, AddRentalSerializer
+from .filters import ProductFilter
+from .pagination import DefaultPagination
 
 
 class CategoryViewSet(ModelViewSet):
@@ -19,8 +24,12 @@ class CategoryViewSet(ModelViewSet):
         return super().destroy(request, *args, **kwargs)
 
 
+
 class BookViewSet(ModelViewSet):
-    queryset = Book.objects.prefetch_related('images').all()
+    queryset = Book.objects.select_related('category').prefetch_related('images').all()
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = ProductFilter
+    pagination_class = DefaultPagination
     
     def get_serializer_class(self):
         if self.request.method in ['POST', 'PATCH', 'PUT']:
@@ -49,3 +58,18 @@ class BookImageViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {'book_id': self.kwargs['book_pk']}
+
+
+class CustomerViewSet(ModelViewSet):
+    serializer_class = CustomerSerializer
+    queryset = Customer.objects.all()
+
+
+class RentalViewSet(ModelViewSet):
+    queryset = Rental.objects.all()
+
+
+    def get_serializer_class(self):
+        if self.request.method in ['POST', 'PATCH', 'PUT']:
+            return AddRentalSerializer
+        return RentalSerializer
